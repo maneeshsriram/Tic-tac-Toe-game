ed9c6b5e69a9807ea60d7845bfc6d1afac89d2d1
Maneesh <maneeshsriram123@gmail.com>
2021-04-06 21:35:38 +0530

    Add simple animation

--- src/App.js
+++ src/App.js
@@ -11,32 +11,34 @@ function App() {
   const [isCross, setIsCross] = useState(true)
   const [winMessage, setWinMessage] = useState("")
 
+
   const CheckisWinner = () => {
-    if (itemsBox[0] != 'empty' && itemsBox[0] === itemsBox[1] && itemsBox[0] === itemsBox[2]) {
-      setWinMessage(`${itemsBox[0]} WINS`)
+    if (itemsBox[0] !== 'empty' && itemsBox[0] === itemsBox[1] && itemsBox[0] === itemsBox[2]) {
+      setWinMessage(`${itemsBox[0]} won`)
     }
-    if (itemsBox[3] != 'empty' && itemsBox[3] === itemsBox[4] && itemsBox[3] === itemsBox[5]) {
-      setWinMessage(`${itemsBox[3]} WINS`)
+    if (itemsBox[3] !== 'empty' && itemsBox[3] === itemsBox[4] && itemsBox[3] === itemsBox[5]) {
+      setWinMessage(`${itemsBox[3]} won`)
     }
-    if (itemsBox[6] != 'empty' && itemsBox[6] === itemsBox[7] && itemsBox[6] === itemsBox[8]) {
-      setWinMessage(`${itemsBox[6]} WINS`)
+    if (itemsBox[6] !== 'empty' && itemsBox[6] === itemsBox[7] && itemsBox[6] === itemsBox[8]) {
+      setWinMessage(`${itemsBox[6]} won`)
     }
-    if (itemsBox[0] != 'empty' && itemsBox[0] === itemsBox[3] && itemsBox[0] === itemsBox[6]) {
-      setWinMessage(`${itemsBox[0]} WINS`)
+    if (itemsBox[0] !== 'empty' && itemsBox[0] === itemsBox[3] && itemsBox[0] === itemsBox[6]) {
+      setWinMessage(`${itemsBox[0]} won`)
     }
-    if (itemsBox[1] != 'empty' && itemsBox[1] === itemsBox[4] && itemsBox[1] === itemsBox[7]) {
-      setWinMessage(`${itemsBox[1]} WINS`)
+    if (itemsBox[1] !== 'empty' && itemsBox[1] === itemsBox[4] && itemsBox[1] === itemsBox[7]) {
+      setWinMessage(`${itemsBox[1]} won`)
     }
-    if (itemsBox[2] != 'empty' && itemsBox[2] === itemsBox[5] && itemsBox[2] === itemsBox[8]) {
-      setWinMessage(`${itemsBox[2]} WINS`)
+    if (itemsBox[2] !== 'empty' && itemsBox[2] === itemsBox[5] && itemsBox[2] === itemsBox[8]) {
+      setWinMessage(`${itemsBox[2]} won`)
     }
-    if (itemsBox[0] != 'empty' && itemsBox[0] === itemsBox[4] && itemsBox[0] === itemsBox[8]) {
-      setWinMessage(`${itemsBox[0]} WINS`)
+    if (itemsBox[0] !== 'empty' && itemsBox[0] === itemsBox[4] && itemsBox[0] === itemsBox[8]) {
+      setWinMessage(`${itemsBox[0]} won`)
     }
-    if (itemsBox[2] != 'empty' && itemsBox[2] === itemsBox[5] && itemsBox[2] === itemsBox[6]) {
-      setWinMessage(`${itemsBox[2]} WINS`)
+    if (itemsBox[2] !== 'empty' && itemsBox[2] === itemsBox[4] && itemsBox[2] === itemsBox[6]) {
+      setWinMessage(`${itemsBox[2]} won`)
     }
   }
+
 
   const reload = () => {
     setWinMessage("")
@@ -44,8 +46,8 @@ function App() {
     setIsCross(true)
   }
 
-  const writeItem = pos => {
 
+  const writeItem = pos => {
     if (winMessage) {
       return toast(winMessage, { type: "success" })
     }
@@ -57,7 +59,6 @@ function App() {
     else {
       return toast("already filled", { type: "error" })
     }
-
     CheckisWinner()
   }
 
@@ -67,7 +68,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1 className="text-center mt-4 text-light text-uppercase heading">Tic Tac Toe</h1>
+      <h1 className="animate__animated animate__fadeIn heading text-center mt-4 text-light text-uppercase heading">Tic Tac Toe</h1>
 
       <div className="container">
 
@@ -86,13 +87,12 @@ function App() {
 
 
         {(winMessage) ?
-
           <div className="text-center mt-4">
-            <h1 className="text-success my-3">{winMessage}</h1>
+            <h1 className="text-success my-3 text-uppercase">{winMessage}</h1>
             <button className="btn btn-primary btn-lg" onClick={reload}>RELOAD</button>
           </div>
           :
-          <h1 className="text-success text-center text-uppercase">{isCross ? 'Cross turn' : 'Circle Turn'}</h1>
+          <h1 className="text-primary text-center text-uppercase turn">{isCross ? 'Cross turn' : 'Circle Turn'}</h1>
         }
 
 
@@ -100,19 +100,16 @@ function App() {
 
         <div className="row">
           <div className="col-md-6 offset-md-3 mt-5">
-
             <div className="row">
 
               {itemsBox.map((item, index) => (
-                <div className="col-md-4
-                " onClick={() => writeItem(index)}>
+                <div className="col-md-4 animate__animated animate__bounceIn icons" onClick={() => writeItem(index)}>
                   <Icons name={item} />
                 </div>
               ))
               }
 
             </div>
-
           </div>
         </div>
       </div>
